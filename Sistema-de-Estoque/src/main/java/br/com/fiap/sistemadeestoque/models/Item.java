61aa10a517aefcd496b255a9fafea47c7e1d3642
DanielTsuyoshi <110398603+DanielTsuyoshi@users.noreply.github.com>
2023-03-20 15:42:39 -0300

    Add files via upload

---
+++ Sistema-de-Estoque/src/main/java/br/com/fiap/sistemadeestoque/models/Item.java
@@ -0,0 +1,66 @@
+package br.com.fiap.sistemadeestoque.models;
+
+import java.math.BigDecimal;
+
+public class Item {
+    
+    private Long id;
+    private String produto;
+    private BigDecimal valor;
+    private String marca;
+    private String descricao;
+    
+
+    public Item(String produto, BigDecimal valor, String marca, String descricao) {
+        this.produto = produto;
+        this.valor = valor;
+        this.marca = marca;
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [produto=" + produto + ", valor=" + valor + ", marca=" + marca + ", descricao=" + descricao + "]";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
